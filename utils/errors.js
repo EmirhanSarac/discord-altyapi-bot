3c2cbee01522f49608e842fcfe69de0412f1a226
Emirhan Saraç <42975466+EmirhanSarac@users.noreply.github.com>
2019-01-12 18:18:25 +0300

    Add files via upload

---
+++ utils/errors.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+let config = require("../botconfig.json");
+
+module.exports.noPerms = (message, perm) => {
+    let embed = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setTitle("Insufficient Permission")
+        .setColor(config.red)
+        .addField("Permission needed", perm);
+
+    message.channel.send(embed).then(m => m.delete(5000));
+}
+
+module.exports.equalPerms = (message, user, perms) => {
+
+    let embed = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setColor(config.red)
+        .setTitle("Error")
+        .addField(`${user} has perms`, perms);
+
+    message.channel.send(embed).then(m => m.delete(5000));
+
+}
+
+module.exports.botuser = (message) => {
+    let embed = new Discord.RichEmbed()
+        .setTitle("Error")
+        .setDescription("You cannot ban a bot.")
+        .setColor(config.red);
+
+    message.channel.send(embed).then(m => m.delete(5000));
+}
+
+module.exports.cantfindUser = (channel) => {
+    let embed = new Discord.RichEmbed()
+        .setTitle("Error")
+        .setDescription("Could not find that user.")
+        .setColor(config.red);
+
+    channel.send(embed).then(m => m.delete(5000));
+}
+
+module.exports.noReason = (channel) => {
+    let embed = new Discord.RichEmbed()
+        .setTitle("Error")
+        .setDescription("Please supply a reason.")
+        .setColor(config.red);
+
+    channel.send(embed).then(m => m.delete(5000));
+}
