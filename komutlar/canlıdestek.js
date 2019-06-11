18ed88a3f2363b552a68ec9d7166b59d2e45a2d6
Emirhan Saraç <42975466+EmirhanSarac@users.noreply.github.com>
2019-06-11 10:53:54 +0300

    Create canlıdestek.js

---
+++ komutlar/canlıdestek.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, params) => {
+    if(message.channel.type == "dm")  return;
+  if(message.channel.type !== "text") return;
+  var channel = client.channels.find('id', '540269834835853340')
+    const asdf = await client.channels.get(message.channel.id).createInvite()
+  message.delete();
+  const embed = new Discord.RichEmbed()
+  .setTitle("»  Bot | Canlı Destek")
+  .setDescription("**Canlı Desteği kullandğın için teşekkür ederiz, Seninle ekibim ilgilenicektir lütfen bekle!**")
+  .setFooter("30 Saniye içinde geri dönülmezse lütfen iletişime geçin <@485741578615521291>")
+ message.channel.send(embed)
+      const invite = new Discord.RichEmbed()
+  .setAuthor("» Canlı Destek | Talep")
+  .addField('**» Kullanıcı Adı: **', message.author.username + '#' + message.author.discriminator)
+  .addField('**» Sunucu Adı: **', message.guild.name)
+  .setDescription(asdf.url)
+      channel.send(invite)
+};
+  
+  exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['canlıdestek'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'canlı-destek',
+  description: 'Canlı Destek Tablebi Oluşturur.',
+  usage: 'g!canlı-destek'
+};
