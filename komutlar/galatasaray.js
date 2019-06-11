ea81afe47e22760127ea5a333fe23cdf7a87316b
Emirhan Saraç <42975466+EmirhanSarac@users.noreply.github.com>
2019-06-11 10:54:30 +0300

    Create galatasaray.js

---
+++ komutlar/galatasaray.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => { 
+              if(message.channel.type == "dm")  return;
+  if(message.channel.type !== "text") return;
+    var Jimp = require("jimp");
+    const Discord = require('discord.js');
+    let img    = Jimp.read(message.mentions.users.first() ? message.mentions.users.first().avatarURL : message.author.avatarURL),
+    moldura = Jimp.read("https://cdn.discordapp.com/attachments/496412678563037186/497368037607866378/gs.png");
+    Promise.all([img, moldura]).then(imgs => {
+    let moldura = imgs[1],
+        img    = imgs[0];
+    moldura.resize(720, 620);  
+    img.resize(720, 615) 
+    img.composite(moldura, 0, 0).getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+        if (!err) 
+        message.channel.send(new Discord.Attachment(buffer));        
+    });
+});
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['gs', 'galatasaray'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'gs',
+  description: 'gs',
+  usage: 'gs'
+};
