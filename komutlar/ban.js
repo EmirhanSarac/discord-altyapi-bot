7a00471504738dbee84ef67cc69d11c766e252e3
crosswashere <42975466+EmirhanSarac@users.noreply.github.com>
2020-05-05 20:31:16 +0300

    Update ban.js

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 const fs = require('fs');
 
 exports.run = (client, message, args) => {
-
-  const db = require('quick.db');
   
 
     
@@ -13,8 +11,6 @@ exports.run = (client, message, args) => {
   let user = message.mentions.users.first();
   let reason = args.slice(1).join(' ');
   //let modLog = JSON.parse(fs.readFileSync("./jsonlar/mLog.json", "utf8"));
-  if (db.has(`mLog_${message.guild.id}`) === false) return message.reply('Mod log kanalı ayarlanmamış');
-  let modlog = message.guild.channels.get(db.fetch(`mLog_${message.guild.id}`).replace("<#", "").replace(">", ""));
   if (message.mentions.users.size < 1) return message.reply('Lütfen banlamak istediğiniz üyeyi etiketleyin');
   if (reason.length < 1) return message.reply('Lütfen sebep giriniz');
   if (user.id === message.author.id) return message.reply('Kendinimi banlayacaksın?');
@@ -22,14 +18,7 @@ exports.run = (client, message, args) => {
 			return message.reply(`Bu kişinin senin rollerinden/rolünden daha yüksek rolleri/rolü var.`);
 		}*/
   //if (!message.guild.member(user).bannable) return message.channel.send(`Bu kişiyi sunucudan yasaklayamıyorum çünkü \`benden daha yüksek bir role sahip\` ya da \`bana gerekli yetkileri vermedin\`.`);
-  
-  const embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .addField('İşlem', 'Ban')
-  .addField('Banlanan üye', `${user.tag} (${user.id})`)
-  .addField('Banlayan yetkili', `${message.author.username}#${message.author.discriminator}`)
-  .addField('Ban sebebi', "```" + reason + "```")
-  modlog.send(embed);
+
   
    //if (!message.guild.member(user).bannable) return message.reply('Yetkilileri yasaklayamam!');
   message.guild.ban(user, 2);
