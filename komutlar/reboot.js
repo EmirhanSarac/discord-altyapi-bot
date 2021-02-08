d1ace0bd367d55ce321201b9ad831b1300d65d0d
codemirhan <42975466+EmirhanSarac@users.noreply.github.com>
2021-02-08 16:52:54 +0300

    Update reboot.js

--- komutlar/reboot.js
+++ komutlar/reboot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const moment = require('moment');
 
 exports.run = (client, message, args) => {
-message.channel.sendMessage(' **Botun Yeniden Başlatılmasını Onaylıyor Musun ?**')
+message.channel.send(' **Botun Yeniden Başlatılmasını Onaylıyor Musun ?**')
 .then(() => {
   message.channel.awaitMessages(response => response.content === "evet", {
     max: 1,
@@ -10,26 +10,25 @@ message.channel.sendMessage(' **Botun Yeniden Başlatılmasını Onaylıyor Musu
     errors: ['time'],
   })
   .then((collected) => {
-      message.channel.sendMessage('  **Yeniden Başlıyorum**   ').then(message => {
+      message.channel.send('  **Yeniden Başlıyorum**   ').then(message => {
       console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] :space_invader: **Bot Yeniden Başlatılıyor** :space_invader:`)
       process.exit(1);
     }).catch(console.error)
     })
     .catch(() => {
-      message.channel.sendMessage(' `Yeniden Başlama İşlemini İptal Ettim` ');
+      message.channel.send(' `Yeniden Başlama İşlemini İptal Ettim` ');
     });
 });
 };
 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
   aliases: ['yenile','yb'],
-  permLevel: 4
+  permLevel: 4,
+  kategori: 'Sahip'
 };
 
 exports.help = {
   name: 'reboot',
-  description: '[YAPIMCI]',
+  description: 'Botu yeniden başlatırsınız',
   usage: 'reboot'
 };
