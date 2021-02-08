8d7817ff797a08d5f45fca23342a0c3e81845e32
codemirhan <42975466+EmirhanSarac@users.noreply.github.com>
2021-02-08 16:44:09 +0300

    Update eventLoader.js

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,5 +1,4 @@
 const reqEvent = (event) => require(`../events/${event}`);
 module.exports = client => {
-  client.on('ready', () => reqEvent('ready')(client));
   client.on('message', reqEvent('message'));
 };
